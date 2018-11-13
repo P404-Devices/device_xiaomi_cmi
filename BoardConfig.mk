521e93ab316459ddb662a55130b0146fafdbc374
Bruce Levy <blevy@codeaurora.org>
2018-11-13 12:15:36 -0800

    kona: Switch console from DCC to serial
    Update cmdline to use serial console instead of DCC.
    
    Change-Id: Iadb86bb52fbc047a41ce8f1a66abda316705d5ac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_VENDOR_KERNEL_MODULES := \
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
 
-BOARD_KERNEL_CMDLINE := console=hvc0 androidboot.hardware=qcom androidboot.console=hvc0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x988000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
