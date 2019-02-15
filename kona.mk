85177220ebbad5c9afe35ddd249dd16d9dac3da5
Aalique Grahame <agrahame@codeaurora.org>
2019-02-15 13:33:39 -0800

    kona: enable libprocessgorup
    Enable libprocessgroup for audio
    
    CRs-Fixed: 2399939
    Change-Id: I838cf7486f5e301627ff87f344c01955bc9bdc3a

--- kona.mk
+++ kona.mk
@@ -200,6 +200,7 @@ KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTAL
 -include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/kona/kona.mk
 
 USE_CUSTOM_AUDIO_POLICY := 0
+USE_LIB_PROCESS_GROUP := true
 
 # Video configuration file
 -include $(TOPDIR)hardware/qcom/media/conf_files/kona/kona.mk
