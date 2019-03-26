128aa74c0c9e0945dbdb2d361a76ac15cb65ff9e
Divya Sharma <divyash@codeaurora.org>
2019-03-26 11:25:42 -0700

    Add missing PRODUCT_HOST_PACKAGES Resolve "Missing modules from PRODUCT_HOST_PACKAGES"
    Bug: 129156971
    Change-Id: I588b75e07ccb64e0d1cc90c065d4f1e09df2f8ff

--- kona.mk
+++ kona.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += update_engine \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload \
+    configstore_xmlparser
+
 # QRTR related packages
 PRODUCT_PACKAGES += qrtr-ns
 PRODUCT_PACKAGES += qrtr-lookup
