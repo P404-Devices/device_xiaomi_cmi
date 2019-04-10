1d8d18951be6ecdb85e697066380f593b2241d71
Ravi Kiran Kasula <rkasul@codeaurora.org>
2019-04-10 11:31:03 +0530

    Remove brillo_update_payload from target PRODUCT_PACKAGES
    brillo_update_payload is already under PRODUCT_HOST_PACKAGES,
    remove it from target PRODUCT_PACKAGES.
    
    Change-Id: Ifc2d64ab2f85703654865df98fa7d5b3360c5ee9

--- kona.mk
+++ kona.mk
@@ -129,7 +129,6 @@ PRODUCT_PACKAGES += update_engine \
     update_engine_client \
     update_verifier \
     bootctrl.kona \
-    brillo_update_payload \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
