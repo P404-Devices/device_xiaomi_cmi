419622ef0ed50b447384dabe7d8e35e926d47278
Mulugeta Engdaw <mengdaw@codeaurora.org>
2019-06-05 16:47:22 -0700

    Initial changes for setting up dynamic partitions
    - Dynamic partition currently set to TRUE
    - System and vendor image size will be disabled in dynamic partition
    mode
    - Recovery image is required in Dynamic partition for AB to support
    first stage mount.
    - Support for Super image creation
    - fastbootd is added to product packages to support individual flashing
    of logical images
    - BOARD_USES_RECOVERY_AS_BOOT as boot should contain first stage ramdisk
    - Dynamic partition fstab is added for logical partition mounting
    -Add ODM image to the dynamic partition
    
    Change-Id: Id121162906eecf640b7db1e7e119b02a347b63cf

--- kona.mk
+++ kona.mk
@@ -25,11 +25,39 @@ TARGET_SKIP_OTA_PACKAGE := true
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 
+
+#####Dynamic partition Handling
+###
+#### Turning this flag to TRUE will enable dynamic partition/super image creation.
+#ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
+BOARD_DYNAMIC_PARTITION_ENABLE ?=true
+#endif
+
+ifneq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
 # Enable chain partition for system, to facilitate system-only OTA in Treble.
 BOARD_AVB_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_SYSTEM_ROLLBACK_INDEX := 0
 BOARD_AVB_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+PRODUCT_BUILD_ODM_IMAGE := false
+else
+PRODUCT_BUILD_ODM_IMAGE := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += fastbootd
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_dynamic_partition.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+BOARD_AVB_VBMETA_SYSTEM := system
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+endif
+
+#####Dynamic partition Handling
+
+
+
+# Enable AVB 2.0
+BOARD_AVB_ENABLE := true
 
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
