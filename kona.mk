99d66c604cd64a8e19efbf6d7ac855379570474f
Jignesh Mehta <jigneshm@codeaurora.org>
2019-09-10 17:07:33 -0700

    Kona: set dalvik.vm.heapgrowthlimit to correct value
    1. Set TARGET_DEFINES_DALVIK_HEAP to true
    If TARGET_DEFINES_DALVIK_HEAP is not set to true,
    then it calls phone-xhdpi-1024-dalvik-heap.mk.
    
    2. Inherit all except heap growth limit from
    frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk
    
    3. Set dalvik.vm.heapgrowthlimit to 256m
    
    This will set dalvik.vm.heapgrowthlimit to correct value.
    
    CRs-fixed: 2500482
    
    Change-Id: I7bc95b74da874098e72d0478f1502c71b9e6b880

--- kona.mk
+++ kona.mk
@@ -71,8 +71,17 @@ TARGET_DISABLE_DISPLAY := false
 # privapp-permissions whitelisting (To Fix CTS :privappPermissionsMustBeEnforced)
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 
+TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, device/qcom/qssi/common64.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+#Product property overrides to configure the Dalvik heap
+PRODUCT_PROPERTY_OVERRIDES  += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapgrowthlimit=256m
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
 
 ###########
 # Target naming
