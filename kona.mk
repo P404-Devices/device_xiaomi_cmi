cf2350c98daaa852f8b50a52756f55fe0ab7d624
Jignesh Mehta <jigneshm@codeaurora.org>
2020-02-20 16:09:12 -0800

    Missing backslash for dalvik.vm.heapgrowthlimit
    This change add backslash.
    
    Change-Id: I3383066a7281ac9ebb6afbf0727d292f4fa14468

--- kona.mk
+++ kona.mk
@@ -87,7 +87,7 @@ $(call inherit-product, device/qcom/qssi/common64.mk)
 PRODUCT_PROPERTY_OVERRIDES  += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heapgrowthlimit=256m
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
