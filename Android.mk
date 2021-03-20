0ed25e69cb1a35ca49b8b37718d30b6d07a6be6c
Akash Srivastava <akashniki@gmail.com>
2021-03-20 09:50:13 +0100

    umi: Call makefiles

--- Android.mk
+++ Android.mk
@@ -2,3 +2,9 @@
 # This empty Android.mk file exists to prevent the build system from
 # automatically including any other Android.mk files under this directory.
 #
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter umi,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
