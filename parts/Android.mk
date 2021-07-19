ff9d29f5a9b241f9fe92fcaf7b850fae04cda437
Akash Srivastava <akashniki@gmail.com>
2021-07-19 15:17:26 +0200

    cmi: Drop touchfeature impl of dt2w

--- parts/Android.mk
+++ parts/Android.mk
@@ -23,10 +23,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    vendor.xiaomi.hardware.displayfeature-V1.0-java \
-    vendor.xiaomi.hardware.touchfeature-V1.0-java
-
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,30 +20,17 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
-    public static final String SHAREDD2TW = "sharadeD2TW";
-    private ITouchFeature mTouchFeature;
-
     @Override
     public void onReceive(final Context context, Intent intent) {
-    //Micro-Service to restore sata of dt2w on reboot
-    SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
-    try {
-        mTouchFeature = ITouchFeature.getService();
-        mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
-    } catch (Exception e) {
-        // Do nothing
-    }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
     }
