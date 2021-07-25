508c0f0135c39400f61079ce5367e90472b98824
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-25 22:19:15 +0800

    cmi: fod: Trigger screen backlight reset after release
    * This is an hack to reset brightness to previous state before FOD enrolling
    * Until we found a fix in kernel, this is more than enough
    
    Change-Id: I10b90e3659c33d8bc0f85d1831ce12eba055a634

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -18,6 +18,7 @@
 
 #include "FingerprintInscreen.h"
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 #include <fstream>
 #include <cmath>
@@ -37,6 +38,11 @@
 #define TOUCH_FOD_ENABLE 10
 
 #define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+#define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
+
+using ::android::base::ReadFileToString;
+using ::android::base::WriteStringToFile;
 
 #define FOD_SENSOR_X 439
 #define FOD_SENSOR_Y 1806
@@ -67,6 +73,18 @@ static bool readBool(int fd) {
     }
 
     return c != '0';
+}
+
+// Read value from path and close file.
+static uint32_t ReadFromFile(const std::string& path) {
+    std::string content;
+    ReadFileToString(path, &content, true);
+    return std::stoi(content);
+}
+
+// Write value to path and close file.
+static bool WriteToFile(const std::string& path, uint32_t content) {
+    return WriteStringToFile(std::to_string(content), path);
 }
 
 FingerprintInscreen::FingerprintInscreen() {
@@ -125,6 +143,7 @@ Return<void> FingerprintInscreen::onPress() {
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    WriteToFile(BRIGHTNESS_PATH, ReadFromFile(BRIGHTNESS_PATH));
     xiaomiTouchFeatureService->resetTouchMode(TOUCH_FOD_ENABLE);
     return Void();
 }
