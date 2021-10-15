b9112e0251e23fbdb86069df17a1f55e3001d30a
Akash Srivastava <akashniki@gmail.com>
2021-10-15 14:03:53 +0200

    cmi: Build display-hal packages from source
    * No need to bloat device tree with those packages

--- device.mk
+++ device.mk
@@ -161,26 +161,10 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 # Display
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
-    android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    gralloc.kona \
-    libtinyxml \
-    libvulkan \
-    memtrack.kona \
-    vendor.display.config@1.11.vendor \
-    vendor.display.config@1.5 \
-    vendor.display.config@2.0 \
-    vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.1.vendor
+-include hardware/qcom-caf/sm8250/display/config/display-board.mk
+-include hardware/qcom-caf/sm8250/display/config/display-product.mk
+include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
+include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
 
 # DRM
 PRODUCT_PACKAGES += \
