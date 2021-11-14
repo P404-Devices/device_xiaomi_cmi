1153e3eb64bf2b10f524789c4f3c9855389cb6bc
Akash <akashniki@gmail.com>
2021-11-14 08:46:44 +0100

    cmi: update blobs from MIUI 12.5.10.0.RJBCNXM

--- aosp_cmi.mk
+++ aosp_cmi.mk
@@ -28,4 +28,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cmi/cmi:11/RKQ1.200826.002/V12.5.8.0.RJBCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cmi/cmi:11/RKQ1.200826.002/V12.5.10.0.RJBCNXM:user/release-keys
