42b5557163f8bfd60ac3636955ead5f1455a7f56
chaptsand <chaptsand@gmail.com>
2022-04-28 20:25:11 +0800

    cmi: Build com.dsi.ant@1.0.vendor from source
    * Also drop ANT+ blobs that we don't need
    
    Change-Id: I501f2685215fd616ba1f420f5e78550ca50734cf

--- device.mk
+++ device.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
+    com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
