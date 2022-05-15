3ad5eec749fd54f40daafefb498adb9123551dde
Akash Srivastava <akashniki@gmail.com>
2022-05-15 20:56:07 +0530

    cmi: Yeet soundtrigger
    * Its crashing and causing audio to lag/cutt off every few seconds

--- device.mk
+++ device.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.kona \
