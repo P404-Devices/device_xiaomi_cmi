1023de32200ae2349c7b5f7b1d55bd2a29286918
Akash Srivastava <akashniki@gmail.com>
2022-05-29 19:21:54 +0530

    cmi: Move privapp-permissions-qti to proprietary-files
    * No need to bloat tree

--- device.mk
+++ device.mk
@@ -320,7 +320,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # RIL
