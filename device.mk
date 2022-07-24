19142f5a48d6467a0ce2cbed2039ae4a5b193839
Akash Srivastava <akashniki@gmail.com>
2022-07-24 19:52:33 +0700

    Revert "cmi: Yeet soundtrigger"
    This reverts commit c18849a36c7cee79a610a63e882d21e6e1d087ae.

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.kona \
