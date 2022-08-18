28fb3dcb10d5ee9d1645fd3e26b6a08b1bea79e8
tarsin <yuanqingxiang233@163.com>
2022-08-18 10:00:01 -0400

    cas: Add shim for missing symbol in com.mi.node.watermark.so
    Change-Id: Idf5fd374ab99a444b14010963126267b18db58c1

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-watermarkshim
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
