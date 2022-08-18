5d7720253b04958d0227ef4ad6be35024dd8f673
Josh Wu <joshwu@google.com>
2022-08-18 21:10:04 +0700

    cmi: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
 BOARD_HAVE_QCOM_FM := false
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
