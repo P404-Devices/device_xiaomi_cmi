ff6b758ab6c87185134c12961d02b94ad88fb8b9
LuK1337 <priv.luk@gmail.com>
2022-09-13 03:15:44 +0700

    cmi: Build the ANT+ service
    Change-Id: Ief5b8e47abf7a5daf8562ac0905248b9418740a2

--- device.mk
+++ device.mk
@@ -71,6 +71,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
@@ -122,7 +127,6 @@ BOARD_HAVE_QCOM_FM := false
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
-    com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
