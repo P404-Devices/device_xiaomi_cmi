e4298457c9e2ae04eac16e65b84b38bf9e2277a9
Akash Srivastava <akashniki@gmail.com>
2022-09-22 14:43:39 +0700

    cmi: Allow DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE to include other entries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.xiaomi_cm
 
 # HIDL
 DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/vintf/vendor_framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
